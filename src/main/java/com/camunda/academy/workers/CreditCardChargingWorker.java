a93cbe046a72fa4289e87761c83b610b276747cf
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2024-10-08 13:03:04 +0200

    ex6

--- src/main/java/com/camunda/academy/workers/CreditCardChargingWorker.java
+++ src/main/java/com/camunda/academy/workers/CreditCardChargingWorker.java
@@ -3,18 +3,34 @@ package com.camunda.academy.workers;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.camunda.academy.services.CreditCardService;
 
 @Component
 public class CreditCardChargingWorker {
 
   Logger LOGGER = LoggerFactory.getLogger(CreditCardChargingWorker.class);
 
+  @Autowired CreditCardService creditCardService;
+
   @JobWorker(type = "credit-card-charging")
   public void handleCreditCardCharging(final JobClient jobClient, final ActivatedJob job) {
     LOGGER.info("Task definition type: " + job.getType());
+    Map<String, Object> variables = job.getVariablesAsMap();
+    String cardNumber = variables.get("cardNumber").toString();
+    String cvc = variables.get("cvc").toString();
+    String expiryDate = variables.get("expiryDate").toString();
+    Double amount = Double.valueOf(variables.get("openAmount").toString());
+
+    creditCardService.chargeAmount(cardNumber, cvc, expiryDate, amount);
+    
     jobClient.newCompleteCommand(job).send().join();
 
   }
--- src/main/java/com/camunda/academy/workers/CreditDeductionWorker.java
+++ src/main/java/com/camunda/academy/workers/CreditDeductionWorker.java
@@ -3,19 +3,36 @@ package com.camunda.academy.workers;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.camunda.academy.services.CustomerService;
 
 @Component
 public class CreditDeductionWorker {
 
+  @Autowired CustomerService customerService;
   Logger LOGGER = LoggerFactory.getLogger(CreditCardChargingWorker.class);
 
   @JobWorker(type = "credit-deduction")
   public void handleCreditCardCharging(final JobClient jobClient, final ActivatedJob job) {
     LOGGER.info("Task definition type: " + job.getType());
-    jobClient.newCompleteCommand(job).send().join();
+
+    Map<String, Object> variables = job.getVariablesAsMap();
+    String customerId = variables.get("customerId").toString();
+    Double orderTotal = Double.valueOf(variables.get("orderTotal").toString());
+
+    double openAmount = customerService.deductCredit(customerId, orderTotal);
+
+    //variables.put("customerCredit", customerCredit);
+    variables.put("openAmount", openAmount);
+
+    jobClient.newCompleteCommand(job).variables(variables).send().join();
 
   }
 }
