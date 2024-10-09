7ceac0d9be459c963c4a3a879428f88512fb3590
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2024-10-09 12:30:51 +0200

    ex8

---
+++ src/main/java/com/camunda/academy/exceptions/InvalidCreditCardException.java
@@ -0,0 +1,8 @@
+package com.camunda.academy.exceptions;
+
+public class InvalidCreditCardException extends Exception {
+
+  public InvalidCreditCardException(String message) {
+    super(message);
+  }
+}
