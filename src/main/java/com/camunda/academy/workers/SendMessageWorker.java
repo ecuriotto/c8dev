7e085b370ea262a688c502c08505c67c7f2f6ea4
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2025-10-09 14:04:06 +0200

    orderId is moved to the process

--- src/main/java/com/camunda/academy/workers/SendMessageWorker.java
+++ src/main/java/com/camunda/academy/workers/SendMessageWorker.java
@@ -24,8 +24,8 @@ public class SendMessageWorker {
     LOGGER.info("Task definition type: " + job.getType());
 
     Map<String, Object> variables = job.getVariablesAsMap();
-    String orderId = generateRandomOrderId(6);
-    variables.put("orderId", orderId);
+    String orderId = variables.get("orderId").toString();
+    // variables.put("orderId", orderId);
 
     zeebeClient.newPublishMessageCommand()
         .messageName("paymentRequestMessage")
@@ -47,16 +47,15 @@ public class SendMessageWorker {
         .correlationKey(variables.get("orderId").toString())
         .send().join();
 
-    jobClient.newCompleteCommand(job)          
-    .send()
-    .exceptionally((
-        throwable -> {
-        throw new RuntimeException("Could not complete job", throwable);
-        }
-    ));
+    jobClient.newCompleteCommand(job)
+        .send()
+        .exceptionally((throwable -> {
+          throw new RuntimeException("Could not complete job", throwable);
+        }));
   }
 
-  // Generates a random order ID with a given length, consisting of letters and/or digits
+  // Generates a random order ID with a given length, consisting of letters and/or
+  // digits
   private String generateRandomOrderId(int length) {
     var stringBuilder = new StringBuilder();
     var random = new Random();
@@ -74,4 +73,3 @@ public class SendMessageWorker {
     return stringBuilder.toString();
   }
 }
-
