719d7175e470cb915c12f0321e0cce17110d5bee
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2025-10-16 12:02:31 +0200

    updated to c8.8

--- src/main/java/com/camunda/academy/workers/CreditDeductionWorker.java
+++ src/main/java/com/camunda/academy/workers/CreditDeductionWorker.java
@@ -1,8 +1,8 @@
 package com.camunda.academy.workers;
 
-import io.camunda.zeebe.client.api.response.ActivatedJob;
-import io.camunda.zeebe.client.api.worker.JobClient;
-import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.client.annotation.JobWorker;
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.worker.JobClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
