bf1b8609df68a6dcf069ebc01ea6fc7e4b6d4abd
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2025-10-16 12:31:43 +0200

    updated to c8.8

--- src/main/java/com/camunda/academy/PaymentApplication.java
+++ src/main/java/com/camunda/academy/PaymentApplication.java
@@ -1,19 +1,13 @@
 package com.camunda.academy;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.camunda.zeebe.client.ZeebeClient;
 
 @SpringBootApplication
 public class PaymentApplication {
 
-	@Autowired ZeebeClient zeebeClient;
-
 	public static void main(final String... args) {
 		SpringApplication.run(PaymentApplication.class, args);
 	}
-
 
 }
